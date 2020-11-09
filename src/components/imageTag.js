ced526e3caebc8d48b03d3de0ef1168143d6580b
syed maaz <syed.m.maaz11@gmail.com>
2020-11-09 18:23:46 +0500

    add loader

---
+++ src/components/imageTag.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState, useContext } from "react";
+import { ImageLoadContext } from "../contexts/imageLoaderContext";
+
+const ImageTag = (props) => {
+  const { imgSrc } = props;
+  const [loadState, setLoadState] = useContext(ImageLoadContext);
+
+  useEffect(() => {
+    setLoadState(loadState + 1);
+    console.log("change");
+  }, [imgSrc]);
+
+  return (
+    <>
+      {loadState}
+      <img src={imgSrc} />
+    </>
+  );
+};
+export default ImageTag;
